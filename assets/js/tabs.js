6f45192f154925e9ae00e3e56b932ac4a3d93bd9
rockn1989 <gold_100@bk.ru>
2021-05-27 00:06:59 +0300

    add tariffs section & tabs

---
+++ assets/js/tabs.js
@@ -0,0 +1,21 @@
+const tabs = document.querySelectorAll(".tabs");
+
+Array.from(tabs).map((tab) => {
+  const tabs = [...tab.querySelectorAll("li")];
+  const tabsContent = tab.nextElementSibling;
+  const tabsContentLi = [...tabsContent.querySelectorAll("li")];
+
+  tab.addEventListener("click", (evt) => {
+    if (evt.target.tagName === "A") {
+      evt.preventDefault();
+      let parentLi = evt.target.closest("li");
+      let tabIdx = tabs.indexOf(parentLi);
+
+      tabs.forEach((li) => li.classList.remove("active-tab"));
+      parentLi.classList.add("active-tab");
+
+      tabsContentLi.forEach((li) => li.classList.remove("active-tab"));
+      tabsContentLi[tabIdx].classList.add("active-tab");
+    }
+  });
+});
